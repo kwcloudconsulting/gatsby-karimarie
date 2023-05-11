1050a261d0587c70b90a0ee405c9aa26516d4bbf
Karimarie Wilson <kwcloudconsulting@gmail.com>
2023-05-11 16:39:03 -0500

    sync

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()}, Built with ❤️ by KWCloudConsulting</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import * as styles from "../styles/global.css";
+
+const Layout = ({ children }) => (
+  <div>
+    <Navbar />
+    <main>{children}</main>
+    <Footer />
+  </div>
+);
+
+export default Layout;
